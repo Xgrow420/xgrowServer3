e367ccfd3639a4083c48407f62c87f8442679619
Synchlor <Synchlor@wp.pl>
2022-05-19 17:55:32 +0200

    + currentUserUtils.py + add getXgrowKeyForCurrentUser in query function pot/repo (for checking and accept)

--- app/repository/pot.py
+++ app/repository/pot.py
@@ -4,17 +4,18 @@ from app.data import models
 from app.schemas import schemas, schemasPot
 from fastapi import HTTPException, status
 
+from app.utils.currentUserUtils import userUtils
 from app.xgrow import XgrowInstance
 from app.xgrow.Climate import Climate
 
 
 def getPots(currentUser: schemas.User, db: Session):
-    pots = db.query(models.Pot).filter(models.Pot.xgrowKey == currentUser.xgrowKey).all()
+    pots = db.query(models.Pot).filter(models.Pot.xgrowKey == userUtils.getXgrowKeyForCurrentUser(currentUser)).all()
     return pots
 
 
 def getPot(index: int, currentUser: schemas.User, db: Session):
-    pot = db.query(models.Pot).filter(models.Pot.xgrowKey == currentUser.xgrowKey,
+    pot = db.query(models.Pot).filter(models.Pot.xgrowKey == userUtils.getXgrowKeyForCurrentUser(currentUser),
                                       models.Pot.index == index).first()
     if not pot:
         # TO Do create mock fan db
@@ -25,7 +26,7 @@ def getPot(index: int, currentUser: schemas.User, db: Session):
 
 
 def createPot(request: schemasPot.PotToModify, currentUser: schemas.User, db: Session):
-    pot = db.query(models.Pot).filter(models.Pot.xgrowKey == currentUser.xgrowKey,
+    pot = db.query(models.Pot).filter(models.Pot.xgrowKey == userUtils.getXgrowKeyForCurrentUser(currentUser),
                                       models.Pot.index == request.index)
 
     if not pot.first():
@@ -48,10 +49,14 @@ def createPot(request: schemasPot.PotToModify, currentUser: schemas.User, db: Se
         db.commit()
         db.refresh(newPot)
         return 'created'
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"[!] Pot for user {currentUser.name} with index {request.index} already exists!")
+
 
 def updatePot(request: schemasPot.PotToModify, currentUser: schemas.User, db: Session):
-    pot = db.query(models.Pot).filter(models.Pot.xgrowKey == currentUser.xgrowKey,
-                                          models.Pot.index == request.index)
+    pot = db.query(models.Pot).filter(models.Pot.xgrowKey == userUtils.getXgrowKeyForCurrentUser(currentUser),
+                                      models.Pot.index == request.index)
 
     if not pot.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
---
+++ app/utils/currentUserUtils.py
@@ -0,0 +1,12 @@
+from app.schemas import schemas
+
+
+class userUtils():
+
+    @staticmethod
+    def getXgrowKeyForCurrentUser(currentUser: schemas.User):
+        '''Return XgrowKey by currentUser schema'''
+        if currentUser.userType:
+            return currentUser.xgrowKey
+        else:
+            return currentUser.name
