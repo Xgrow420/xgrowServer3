08936a8565634f4cd1a0f80cba3bed95a80fe4a2
Marcin Piskorz <marcinpiskorz01@gmail.com>
2022-05-29 18:46:33 +0200

    Dummy WebSocketConnection manager

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+
 from app.data import models
 from app.data.database import engine
 from app.routers import air, customDevice, timerTrigger, endpointUtils
-from app.routers import fan, blog, authentication, user, pot
+from app.routers import fan, blog, authentication, user, pot, webSocketConnection
 
 app = FastAPI()
 
@@ -18,6 +19,7 @@ app.include_router(air.router)
 app.include_router(customDevice.router)
 app.include_router(timerTrigger.router)
 app.include_router(endpointUtils.router)
+app.include_router(webSocketConnection.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
