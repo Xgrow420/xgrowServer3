54c216728c85f12ec454118da68ab474b42f7d1f
Mint Missy <dawidkostka480@gmail.com>
2022-07-26 01:40:51 +0200

    remove cors

--- main.py
+++ main.py
@@ -40,17 +40,6 @@ app.include_router(airSensorTrigger.router)
 def get_status():
     return 'chuj'
 
-origins = ["http://localhost", "http://localhost:4200", "http://127.0.0.1:8000", "http://localhost:8000",
-           "http://127.0.0.1:4200", "http://localhost:4200"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST_LOCATION, port=PORT)
 
